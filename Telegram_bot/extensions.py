c6e759c23068792d6750cc67f6aa1ad531aa608e
Name <zhdankin.1981@mail.ru>
2023-06-27 21:54:40 +0300

    Update extensions.py

--- Telegram_bot/extensions.py
+++ Telegram_bot/extensions.py
@@ -1,16 +1,18 @@
 import requests
 
 
-class APIException(Exception):
-    pass
+class APIException(Exception):#класс исключения,который поможет нам для обработки ошибок
+    pass                      #возникшие при работе с API
 
 
-class CurrencyConverter:
+class CurrencyConverter:#класс для получения курса валют
     @staticmethod
     def get_price(base: str, quote: str, amount: float) -> float:
         url = f"https://api.exchangerate-api.com/v4/latest/{base}"
         response = requests.get(url)
-
+    # base - валюта, цену которой мы хотим узнать, quote - валюта,
+    # в которой мы хотим узнать цену первой валюты, amount - количество первой валюты.    
+    #Метод использует API для получения курса валют и возвращает цену во второй валюте.
         if response.status_code != 200:
             raise APIException(f"Не удалось получить курс {base}")
 
